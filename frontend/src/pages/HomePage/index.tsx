8e47dc17506912aaf075c98fafd806faf2a0f7b5
Daniel Porteous <daniel@dport.me>
2023-05-16 10:05:12 +0100

    Add more info to the home screen about how the tontine works

--- frontend/src/pages/HomePage/index.tsx
+++ frontend/src/pages/HomePage/index.tsx
@@ -2,10 +2,14 @@ import { useWallet } from "@aptos-labs/wallet-adapter-react";
 import {
   Box,
   Center,
+  Divider,
   Flex,
   Image,
   Link,
+  List,
+  ListItem,
   Text,
+  UnorderedList,
   useColorModeValue,
 } from "@chakra-ui/react";
 import { TontineList } from "../../components/TontineList";
@@ -27,8 +31,6 @@ export const HomePage = () => {
 
   const [showingCreateComponent, setShowingCreateComponent] = useState(false);
 
-  const linkColor = useColorModeValue("blue.500", "blue.300");
-
   if (
     network &&
     network.name.toLowerCase() !== state.network_name.toLowerCase()
@@ -49,7 +51,39 @@ export const HomePage = () => {
     );
   }
 
-  const infoComponent = (
+  // Note: If there are more tontines than fit in a single screen, they overflow
+  // beyond the end of the sidebar box downward. I have not been able to fix it.
+  return (
+    <Flex p={3} height="100%" flex="1" overflow="auto">
+      <Box width="21%" borderRight="1px">
+        <TontineList
+          activeTontine={activeTontine}
+          setActiveTontine={setActiveTontine}
+        />
+      </Box>
+      <Box width="79%">
+        {connected && activeTontine ? (
+          <TontineDisplay
+            activeTontine={activeTontine}
+            setActiveTontine={setActiveTontine}
+          />
+        ) : (
+          <Box>
+            <HomeActions
+              showingCreateComponent={showingCreateComponent}
+              setShowingCreateComponent={setShowingCreateComponent}
+            />
+            {showingCreateComponent ? <CreateTontine /> : <InfoComponent />}
+          </Box>
+        )}
+      </Box>
+    </Flex>
+  );
+};
+
+export const InfoComponent = () => {
+  const linkColor = useColorModeValue("blue.500", "blue.300");
+  return (
     <Box
       p={7}
       paddingTop={0}
@@ -97,36 +131,62 @@ export const HomePage = () => {
             Wikipedia
           </Link>
         </Box>
+        <Box paddingTop={3} paddingBottom={5}>
+          <Divider />
+        </Box>
+        <Text>
+          The tontine described here is a variant of the standard tontine that
+          works as described above. In this scheme, people invest funds into a
+          shared fund. It remains locked in the fund until only one member of
+          the tontine remains, at which point they may claim the full sum of the
+          invested funds.
+        </Text>
+        <Box p={2} />
+        <Text paddingBottom={1}>
+          Organizing a tontine on-chain has a variety of interesting properties,
+          be them advantages or disadvantages:
+        </Text>
+        <UnorderedList spacing={2}>
+          <ListItem>
+            In traditional tontines it is difficult to devise a mechanism where
+            the funds can only be retrieved once one member remains. This is
+            easily enforced on chain.
+          </ListItem>
+          <ListItem>
+            Aptos accounts need not necessarily be owned by a single individual.
+            To avoid{" "}
+            <Link
+              color={linkColor}
+              href="https://www.explainxkcd.com/wiki/index.php/538:_Security"
+            >
+              wrench attacks
+            </Link>{" "}
+            they may use a multisigner account, either shared with other
+            individuals, or just sharded in a way that makes it hard for another
+            party to get the full key.
+          </ListItem>
+          <ListItem>
+            Aptos accounts do not strictly map to a single indvidiual. This has
+            interesting implications. For example, a tontine could theoretically
+            outlast generations, with accounts being handed down throughout
+            time.
+          </ListItem>
+        </UnorderedList>
+        <Text paddingTop={3}>
+          Make sure you understand these properties before taking part in a
+          tontine organized through this Move module.
+        </Text>
+        <Text paddingTop={3}>
+          Learn more about the tontine lifecycle{" "}
+          <Link
+            color={linkColor}
+            href="https://github.com/banool/aptos-tontine#standard-tontine-lifecycle"
+          >
+            here
+          </Link>
+          .
+        </Text>
       </Box>
     </Box>
-  );
-
-  // Note: If there are more tontines than fit in a single screen, they overflow
-  // beyond the end of the sidebar box downward. I have not been able to fix it.
-  return (
-    <Flex p={3} height="100%" flex="1" overflow="auto">
-      <Box width="21%" borderRight="1px">
-        <TontineList
-          activeTontine={activeTontine}
-          setActiveTontine={setActiveTontine}
-        />
-      </Box>
-      <Box width="79%">
-        {connected && activeTontine ? (
-          <TontineDisplay
-            activeTontine={activeTontine}
-            setActiveTontine={setActiveTontine}
-          />
-        ) : (
-          <Box>
-            <HomeActions
-              showingCreateComponent={showingCreateComponent}
-              setShowingCreateComponent={setShowingCreateComponent}
-            />
-            {showingCreateComponent ? <CreateTontine /> : infoComponent}
-          </Box>
-        )}
-      </Box>
-    </Flex>
   );
 };
